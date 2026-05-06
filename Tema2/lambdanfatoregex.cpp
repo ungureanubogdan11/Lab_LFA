496f555f7990b81676f0bc992ce24e1b7a3d3d64
ung <ungureanu.bogdan2007@gmail.com>
2026-05-06 18:05:04 +0300

    added tema 2

---
+++ Tema2/lambdanfatoregex.cpp
@@ -0,0 +1,93 @@
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+ifstream fin("inputs/data_lambda.in");
+ofstream fout("outputs/answer_lambda.out");
+
+int nr_stari, stare_init = 0, nr_litere, nr_stari_finale, nr_muchii;
+bool stare_finala[1001];
+
+string star(string r) {
+    if (r == "") return "lambda";
+    if (r == "lambda") return "lambda";
+    if (r.length() == 1) return r + "*";
+    return "(" + r + ")*";
+}
+
+string concat(string r1, string r2) {
+    if (r1 == "" || r2 == "") return "";
+    if (r1 == "lambda") return r2;
+    if (r2 == "lambda") return r1;
+    return r1 + r2;
+}
+
+string reunite(string r1, string r2) {
+    if (r1 == "") return r2;
+    if (r2 == "") return r1;
+    if (r1 == r2) return r1;
+    return "(" + r1 + "|" + r2 + ")";
+}
+
+void solve() {
+    if (!(fin >> nr_stari >> nr_litere)) return;
+    for (int i = 0; i < nr_litere; ++i) {
+        string litera; fin >> litera;
+    }
+    
+    fin >> nr_stari_finale;
+    for (int i = 0; i < nr_stari_finale; ++i) {
+        int stare; fin >> stare;
+        stare_finala[stare] = 1;
+    }
+    
+    fin >> nr_muchii;
+    int MAX_STARI = nr_stari + 2; 
+    vector<vector<string>> adj(MAX_STARI, vector<string>(MAX_STARI, ""));
+    
+    for (int i = 0; i < nr_muchii; ++i) {
+        int src, dest;
+        string lit;
+        fin >> src >> dest >> lit;
+        adj[src][dest] = reunite(adj[src][dest], lit);
+    }
+    
+    int qs = nr_stari;
+    int qf = nr_stari + 1;
+    
+    adj[qs][stare_init] = "lambda";
+    for (int i = 0; i < nr_stari; ++i) {
+        if (stare_finala[i]) {
+            adj[i][qf] = "lambda";
+        }
+    }
+    
+    for (int k = 0; k < nr_stari; ++k) {
+        for (int p = 0; p <= qf; ++p) {
+            if (p == k || adj[p][k] == "") continue;
+            for (int r = 0; r <= qf; ++r) {
+                if (r == k || adj[k][r] == "") continue;
+                
+                string R_pk = adj[p][k];
+                string R_kk = adj[k][k];
+                string R_kr = adj[k][r];
+                string path_through_k = concat(R_pk, concat(star(R_kk), R_kr));
+                
+                adj[p][r] = reunite(adj[p][r], path_through_k);
+            }
+        }
+        for (int i = 0; i <= qf; ++i) {
+            adj[k][i] = "";
+            adj[i][k] = "";
+        }
+    }
+    
+    fout << adj[qs][qf] << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
---
+++ Tema2/pdasim.cpp
@@ -0,0 +1,85 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+ifstream fin("inputs/data_pda.in");
+ofstream fout("outputs/answer_pda.out");
+
+struct Tranzitie {
+    int stare_destinatie;
+    string caractere_noi_stiva;
+};
+
+struct Muchie {
+    char simbol_intrare;
+    char simbol_top_stiva;
+    vector<Tranzitie> optiuni;
+};
+
+int nr_stari, stare_start, nr_finale;
+bool este_finala[1001];
+char simbol_initial_stiva;
+vector<Muchie> muchii[1001];
+
+bool ruleaza_pda(int stare_ac, int poz_cuvant, string cuvant, stack<char> stiva) {
+    if (poz_cuvant == cuvant.length()) {
+        if (este_finala[stare_ac]) return true;
+    }
+
+    for (auto& m : muchii[stare_ac]) {
+        bool poate_citi = (poz_cuvant < cuvant.length() && m.simbol_intrare == cuvant[poz_cuvant]);
+        bool este_lambda = (m.simbol_intrare == 'L');
+
+        if (poate_citi || este_lambda) {
+            if (!stiva.empty() && stiva.top() == m.simbol_top_stiva) {
+                for (auto& opt : m.optiuni) {
+                    stack<char> stiva_noua = stiva;
+                    stiva_noua.pop();
+
+                    if (opt.caractere_noi_stiva != "L") {
+                        for (int i = opt.caractere_noi_stiva.length() - 1; i >= 0; i--) {
+                            stiva_noua.push(opt.caractere_noi_stiva[i]);
+                        }
+                    }
+
+                    int urmatoarea_pozitie = poate_citi ? poz_cuvant + 1 : poz_cuvant;
+                    if (ruleaza_pda(opt.stare_destinatie, urmatoarea_pozitie, cuvant, stiva_noua)) {
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    int alfabet_size, stiva_alfabet_size, nr_muchii;
+    fin >> nr_stari >> alfabet_size >> stiva_alfabet_size;
+    fin >> stare_start >> simbol_initial_stiva;
+    
+    fin >> nr_finale;
+    for (int i = 0; i < nr_finale; i++) {
+        int f; fin >> f;
+        este_finala[f] = true;
+    }
+
+    fin >> nr_muchii;
+    for (int i = 0; i < nr_muchii; i++) {
+        int u, v; char in, top; string rez;
+        fin >> u >> in >> top >> v >> rez;
+        muchii[u].push_back({in, top, {{v, rez}}});
+    }
+
+    string cuvant_test;
+    while (fin >> cuvant_test) {
+        stack<char> s;
+        s.push(simbol_initial_stiva);
+        if (ruleaza_pda(stare_start, 0, cuvant_test, s)) fout << "DA\n";
+        else fout << "NU\n";
+    }
+    return 0;
+}
